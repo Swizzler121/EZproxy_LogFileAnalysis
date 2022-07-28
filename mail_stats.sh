dcff1e0b2b1f6227d8588d1281a8a6a6bc9d832e
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2022-07-28 15:36:10 -0500

    Create mail_stats.sh

---
+++ mail_stats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#variables to use correct command
+LAST_MONTH=`date -d "$(date +%Y-%m-1) -1 month" +%-m`
+THIS_MONTH=`date -d "$(date +%Y-%m-1) 0 month" +%-m`
+
+echo "delay test" && wait 600
+echo "it's been 10 minutes"
