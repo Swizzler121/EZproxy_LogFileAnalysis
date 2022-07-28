af03e502f75c7dcbddf1548299d77a5fa03ba86d
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2022-07-28 17:39:59 -0500

    Update mail_stats.sh

--- mail_stats.sh
+++ mail_stats.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
-#Static Variables
+####################
+##Static Variables##
+####################
 LAST_MONTH_NAME=`date -d "$(date +%Y-%m-1) -1 month" +%B`
 LAST_MONTH_YEARDAY=`date -d "$(date +%Y-%m-1) -1 month" +%Y%m`
 
-#User Editable Variables
+###########################
+##User Editable Variables##
+###########################
 #destination Email Address
 DEST_EMAIL="example@example.com"
+
 #Prefix before Month Name
 SUBJECT_PF="EZProxy Stats"
+
+#EZP-stats ouput folder/file location
+OUTPUT_LOC="/home/ezproxy/ezpstats/output/ezpstat_$LAST_MONTH_YEARDAY.html"
 
 #Program
 python3 /home/ezproxy/ezpstats/ezp-stats.py &
 PROCESS_ID=$!
-echo "waiting for script to finish"
+#Waiting for script to finish
 wait $PROCESS_ID
-echo "sending mail"
-echo "" | mutt -s "$SUBJECT_PF $LAST_MONTH_NAME" $DEST_EMAIL -a /home/ezproxy/ezpstats/output/ezpstat_$LAST_MONTH_YEARDAY.html
+#Sending mail
+echo "" | mutt -s "$SUBJECT_PF $LAST_MONTH_NAME" $DEST_EMAIL -a $OUTPUT_LOC
