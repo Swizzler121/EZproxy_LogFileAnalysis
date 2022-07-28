d4323390c186998cce7980e9142a53c3649ef1cd
Swizzler121 <36831696+Swizzler121@users.noreply.github.com>
2022-07-28 15:45:47 -0500

    Update mail_stats.sh

--- mail_stats.sh
+++ mail_stats.sh
@@ -4,5 +4,6 @@
 LAST_MONTH=`date -d "$(date +%Y-%m-1) -1 month" +%-m`
 THIS_MONTH=`date -d "$(date +%Y-%m-1) 0 month" +%-m`
 
-echo "delay test" && wait 600
+echo "delay test" 
+wait 600
 echo "it's been 10 minutes"
